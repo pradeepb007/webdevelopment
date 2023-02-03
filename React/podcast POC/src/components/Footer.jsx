b7cd30cc1dd24598068289b2618df5746e225382
Pradeep Baddapuri <98583284+pradeepb007@users.noreply.github.com>
2023-02-03 17:02:27 +0530

    delete

--- React/podcast POC/src/components/Footer.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function Footer() {
-  return (
-    <div className='footer text-sm'>&copy; 2023. All Rights Reserved. Powered by Unilever Podcasts</div>
-  )
-}
