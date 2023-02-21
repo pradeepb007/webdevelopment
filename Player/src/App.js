3e745cb95d4a78a99ac86e18524e3779df1b8ad0
Pradeep Baddapuri <98583284+pradeepb007@users.noreply.github.com>
2023-02-21 12:11:29 +0530

    comit

---
+++ Player/src/App.js
@@ -0,0 +1,14 @@
+import React, { useEffect, useRef } from 'react';
+import VideoPlayer from './VideoPlayer';
+
+function App() {
+
+
+  return (
+    <div>
+      <VideoPlayer/>
+  </div>
+  );
+}
+
+export default App;
